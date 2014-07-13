aaa7e3cb0ca91f5c9d73a12a5cccfbe8dddb27b7
Vince Hodges <vhodges@gmail.com>
2014-07-13 07:21:11 -0700

    Display current credit card on payment method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return a string suitable for displaying a CC as last 4 digis
+  def last_four(digits)
+    cc_string = ""
+    (0..2).each do 
+      cc_string += "**** "
+    end
+    cc_string += digits
+  end
+
 end
