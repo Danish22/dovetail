2d7211e286c90ae78fdb88464eeab1ab9b6e135d
Vince Hodges <vhodges@gmail.com>
2014-07-16 14:34:40 -0700

    Added dependent destroys and a before filter to cancel any subscription

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,12 +1,12 @@
 class Space < ActiveRecord::Base
   belongs_to :user
 
-  has_many :user_spaces
+  has_many :user_spaces, dependent: :destroy
   has_many :users, through: :user_spaces
 
-  has_many :created_members, class: Member
+  has_many :created_members, class: Member, dependent: :destroy
 
-  has_many :space_members
+  has_many :space_members, dependent: :destroy
   has_many :members, through: :space_members
 
   # Space is payed for through the connected payment_method
@@ -14,6 +14,8 @@ class Space < ActiveRecord::Base
   belongs_to :payment_method # Plus plan and stripe_subscription_id
 
   scope :missing_payment_method, -> { where(payment_method_id: nil) }
+
+  before_destroy :cancel_subscription
 
   validates :name, presence: true
   validates :address, presence: true
@@ -28,5 +30,11 @@ class Space < ActiveRecord::Base
     ]
   end
 
+  # TODO Maybe handle (somehow) exception.
+  def cancel_subscription
+    unless payment_method.nil?
+      payment_method.cancel_subscription(self)
+    end
+  end
 
 end
