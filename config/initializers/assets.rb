76cbbb560d92968711cae79c4203d09df7f59430
Anthony Penner <penner.anthony@gmail.com>
2014-07-29 21:34:40 -0700

    woot

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,3 @@
 Rails.application.config.assets.precompile += %w( neon-forms.css )
 Rails.application.config.assets.precompile += %w( custom.css )
 Rails.application.config.assets.precompile += %w( jquery-ui-1.10.3.custom.min.css )
-Rails.application.config.assets.precompile += %w( neon-register.js )
