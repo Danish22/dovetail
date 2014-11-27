90745ab562b8cd73feeafdf2e3969656da0d9c6a
Vince Hodges <vhodges@gmail.com>
2014-11-26 16:58:55 -0800

    Moved code from gem into model

--- app/models/member_identity.rb
+++ app/models/member_identity.rb
@@ -1,3 +1,15 @@
-class MemberIdentity < OmniAuth::Identity::Models::ActiveRecord
-  self.table_name = "member_identities"
+class MemberIdentity < ActiveRecord::Base
+  include OmniAuth::Identity::Model
+  include OmniAuth::Identity::SecurePassword
+  
+  has_secure_password
+  
+  def self.auth_key=(key)
+    super
+    validates_uniqueness_of key, :case_sensitive => false
+  end
+  
+  def self.locate(search_hash)
+    where(search_hash).first
+  end
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,6 @@ set :deploy_to, '/home/deploy/dovetailv2'
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
  
