38fba3d87aee56a5b108484e23e7a17b224b477b
Vince Hodges <vhodges@gmail.com>
2014-12-01 05:34:54 -0800

    Ensure tax amount is non nil

--- app/models/invoicing_line_item.rb
+++ app/models/invoicing_line_item.rb
@@ -1,5 +1,9 @@
 class InvoicingLineItem < ActiveRecord::Base
   acts_as_line_item
-
+  before_save :ensure_tax_amount
   belongs_to :ledger_item, class_name: 'InvoicingLedgerItem'
+
+  def ensure_tax_amount
+    self.tax_amount = 0 if self.tax_amount.nil?
+  end
 end
