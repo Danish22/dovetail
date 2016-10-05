a24cb8c2185fa277f82f1c2cdc8a73cd8c6a1e64
Vince Hodges <vhodges@gmail.com>
2016-10-04 17:10:18 -0700

    Typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'ENV['DEFAULT_MAILER_HOST'], :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => ENV['DEFAULT_MAILER_HOST'], :protocol => 'https'}
   
   config.action_mailer.smtp_settings = {
     :address        => ENV['SMTP_ADDRESS'],
